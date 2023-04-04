64e6f71db3a15f13a8050045fa72cacc6759911d
inkarbegim <48454291+inkarbegim@users.noreply.github.com>
2023-04-04 17:07:49 +0100

    Preview of slice FindUsOn

---
+++ .slicemachine/assets/slices/FindUsOn/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/FindUsOn';
+
+export default {
+  title: 'slices/FindUsOn'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"link_title":"national","link":{"link_type":"Web","url":"http://google.com"}}],"primary":{"title":[{"type":"heading2","text":"Remember","spans":[]}]},"id":"_Default","slice_type":"find_us_on"}} />
+_Default.storyName = ''
