4eb4ec8065ec01ead325af4492d8b5c9fb7017d2
inkarbegim <48454291+inkarbegim@users.noreply.github.com>
2023-04-04 17:07:31 +0100

    Preview of slice Team

---
+++ .slicemachine/assets/slices/Team/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Team';
+
+export default {
+  title: 'slices/Team'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"image":{"dimensions":{"width":2545,"height":2545},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1600861194802-a2b11076bc51"},"name":[{"type":"heading2","text":"Produce","spans":[]}],"role":[{"type":"paragraph","text":"Exercitation ex id id. Laboris dolor minim nostrud pariatur aliquip eu labore sit non.","spans":[]}]}],"primary":{"title":[{"type":"heading1","text":"Toward","spans":[]}],"description":[{"type":"paragraph","text":"Eiusmod duis ut cillum.","spans":[]}]},"id":"_Default","slice_type":"hero"}} />
+_Default.storyName = ''
