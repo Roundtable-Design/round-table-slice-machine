cb3bbea95b78cac9c3aa71088c55eb3eaf899782
inkarbegim <inkarbegimkereyeva@gmail.com>
2024-04-11 15:11:50 +0100

    adding h2-h6 for richtext compiler

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,21 @@ const richTextComponents = {
   heading1: ({ children }) => (
     <h1 className="text-base font-bold mb-6">{children}</h1>
   ),
+  heading2: ({ children }) => (
+    <h2 className="text-base font-bold mb-6">{children}</h2>
+  ),
+  heading3: ({ children }) => (
+    <h3 className="text-base font-bold mb-6">{children}</h3>
+  ),
+  heading4: ({ children }) => (
+    <h4 className="text-base font-bold mb-6">{children}</h4>
+  ),
+  heading5: ({ children }) => (
+    <h5 className="text-base font-bold mb-6">{children}</h5>
+  ),
+  heading6: ({ children }) => (
+    <h6 className="text-base font-bold mb-6">{children}</h6>
+  ),
   paragraph: ({ children }) => (
     <p className="text-base font-normal max-w-l">{children}</p>
   ),
