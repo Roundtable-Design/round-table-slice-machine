331ae743b25ad4fae859bdc98322d0a54e1db23f
inkarbegim <inkarbegimkereyeva@gmail.com>
2024-10-23 15:41:38 +0100

    adding spiral shape and reshuffle on objects disappearing

--- customtypes/cursor/objects/FollowingObject.js
+++ customtypes/cursor/objects/FollowingObject.js
@@ -1,16 +1,27 @@
 import { motion } from "framer-motion";
-import useMousePosition from "../utils/useMousePosition";
 
 export default function FollowingObject(props) {
   return (
     <motion.div
-      className={`h-20 w-20`}
+      className="h-20 w-20"
       initial={{ scale: 0, x: props.x, y: props.y }}
       animate={{ scale: 1, x: props.x - 20, y: props.y - 20 }}
-      // exit={{ scale: 0 }}
       transition={{
-        duration: props.movingSpeed,
-        // ease: "easeInOut",
+        x: props.converging
+          ? {
+              type: "spring",
+              stiffness: 50,
+              damping: 20,
+            }
+          : { duration: props.movingSpeed },
+        y: props.converging
+          ? {
+              type: "spring",
+              stiffness: 50,
+              damping: 20,
+            }
+          : { duration: props.movingSpeed },
+        scale: { duration: props.movingSpeed },
       }}
     >
       <motion.svg
@@ -28,13 +39,12 @@ export default function FollowingObject(props) {
         xmlns="http://www.w3.org/2000/svg"
         className="absolute"
       >
-        {/* <motion.path exit={{ scale: 0 }} d={props.paths[0]} fill="#FFFFFF" /> */}
         {props.paths.map((pathData, index) => (
           <motion.path
             key={index}
             exit={{ scale: 0 }}
             d={pathData}
-            fill="#FFFFFF" // Adjust fill or make it dynamic as needed
+            fill="#FFFFFF"
           />
         ))}
       </motion.svg>
--- customtypes/cursor/utils/useMousePosition.js
+++ customtypes/cursor/utils/useMousePosition.js
@@ -1,25 +1,36 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
 export default function useMousePosition() {
-    
-    const [mousePosition, setMousePosition] = useState({x: 0, y: 0});
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
 
-    let clientScrollY = 0;
-    let totalScrollY = 0;
+  //   let clientScrollY = 0;
+  //   let totalScrollY = 0;
 
+  //   const updateMousePosition = (e) => {
+  //     setMousePosition({ x: e.clientX, y: e.clientY });
+  //     //                           -20              -900
+  //   };
+
+  //   useEffect(() => {
+  //     document.addEventListener("mousemove", (e1) => {
+  //       document.addEventListener("mousemove", updateMousePosition);
+  //       return () => {
+  //         document.removeEventListener("mousemove", updateMousePosition);
+  //       };
+  //     });
+  //   }, []);
+
+  useEffect(() => {
     const updateMousePosition = (e) => {
-        setMousePosition({x: e.clientX , y: e.clientY});
-        //                           -20              -900
-    } 
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
 
-    useEffect ( () => {
-        document.addEventListener('mousemove', (e1) => {
-            document.addEventListener('mousemove', updateMousePosition)
-            return () => {
-                document.removeEventListener("mousemove", updateMousePosition)
-            }
-        })
-    },[])
+    document.addEventListener("mousemove", updateMousePosition);
 
-    return mousePosition;
+    return () => {
+      document.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
 }
