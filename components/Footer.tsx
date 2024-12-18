82d606871cf7ec2336785821b167e3337ecb5610
inkarbegim <inkarbegimkereyeva@gmail.com>
2024-12-18 13:00:01 +0000

    code cleaning

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,16 +29,6 @@ export default function Footer({ data }: FooterProps) {
           </div>
         </div>
         <div className="w-auto h-auto">
-          {/* <div
-            className="w-full clutch-widget bg-white p-1 rounded"
-            data-url="https://widget.clutch.co"
-            data-widget-type="2"
-            data-height="45"
-            data-nofollow="true"
-            data-expandifr="true"
-            data-scale="100"
-            data-clutchcompany-id="2017780"
-          ></div> */}
           <ClutchWidget />
         </div>
       </div>
